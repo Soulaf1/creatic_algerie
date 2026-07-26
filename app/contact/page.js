786d67fb4654d84092a4ca4d4822f322c85496c4
Basma <Aijamatine1@gmail.com>
2026-07-26 19:15:38 +0100

    feat: deleting the map

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,13 +1,11 @@
 import ContactHero from "@/components/ContactHero";
 import ContactSection from "@/components/ContactSection";
-import ContactMap from "@/components/ContactMap";
 
 export default function Contact() {
   return (
     <>
       <ContactHero />
       <ContactSection />
-      <ContactMap />
     </>
   );
 }
